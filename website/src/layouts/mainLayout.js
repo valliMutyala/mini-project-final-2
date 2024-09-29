804ee27c5c677dfe875e7d6dad18cf34dd1b4869
crazy0codes <madhanpanja@gmail.com>
2024-09-29 16:30:23 +0530

    commit

--- website/src/layouts/mainLayout.js
+++ website/src/layouts/mainLayout.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer";
 
 export default function MainLayout(){
+    const [location, setLocation] = useState(null);
+    const [data, setData] = useState(null)
+
+    function success(pos){
+        const crd = pos.coords;
+        setLocation([crd.latitude, crd.longitude]);
+    }
+
+    function error(err){
+        console.log(err)
+    }
+
+    useEffect(() => {
+        async function loadData() {
+            if(location){
+                navigator.geolocation.getCurrentPosition(success, error);
+                const request = await fetch('https://localhost:3000/currentlocation' , {
+                    method : "GET",
+                    body : {
+                        location,
+                    }
+                });
+                const res = await request.json();
+                setData(res);
+            }
+        }
+        loadData();
+    }, [])
     return(
         <>
             <Navbar/>
